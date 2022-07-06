466f54ee1d178a2013f62889d0e7c1ec8619ed9c
ag865 <abdulghaffar865@gmail.com>
2022-07-07 01:17:09 +0500

    Bull Removed redis added to providers

--- start/scheduler.js
+++ start/scheduler.js
@@ -1,13 +1,1 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const Bull_1 = __importDefault(global[Symbol.for('ioc.use')]("Rocketseat/Bull"));
-const QueueScheduleNotification_1 = __importDefault(global[Symbol.for('ioc.use')]("App/Jobs/QueueScheduleNotification"));
-Bull_1.default.add(new QueueScheduleNotification_1.default().key, {}, {
-    repeat: {
-        cron: "0 3 * * *"
-    }
-});
 //# sourceMappingURL=scheduler.js.map
