78915b677361d6b30f75c009563ed4e4733736c9
Ahmad <ahmadhussain03@hotmail.com>
2022-07-16 08:54:01 +0500

    Bull Provider Added

--- app/Jobs/QueueScheduleNotification.js
+++ app/Jobs/QueueScheduleNotification.js
@@ -1,1 +1,48 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const Bull_1 = __importDefault(global[Symbol.for('ioc.use')]("Rocketseat/Bull"));
+const ScheduleRoutine_1 = __importDefault(global[Symbol.for('ioc.use')]("App/Models/ScheduleRoutine"));
+const date_fns_1 = require("date-fns");
+const luxon_1 = require("luxon");
+const SendScheduleNotification_1 = __importDefault(require("./SendScheduleNotification"));
+class QueueScheduleNotification {
+    constructor() {
+        this.key = 'QueueScheduleNotification';
+    }
+    async handle() {
+        const currentTime = luxon_1.DateTime.now().toFormat('HH:mm:ss');
+        const todayDate = luxon_1.DateTime.now().toFormat('yyyy-MM-dd');
+        const todayDateNumber = luxon_1.DateTime.now().toFormat('dd');
+        const today = luxon_1.DateTime.now().weekdayLong.toLowerCase();
+        const schedulesQuery = ScheduleRoutine_1.default.query().where('start_time', '>', currentTime).preload('schedule').preload('checkpoint');
+        schedulesQuery.where(subQuery => {
+            subQuery.where(query => {
+                query.where(query => {
+                    query.whereNotNull('check_date').where('repeat', 'Monthly').whereRaw('EXTRACT(DAY FROM check_date) = ?', [todayDateNumber]);
+                }).orWhere(query => {
+                    query.whereNotNull('check_date').where('repeat', 'Yearly').whereRaw('DATE(check_date) = ?', [todayDate]);
+                }).orWhere(query => {
+                    query.whereNull('check_date').where('repeat', 'Daily').whereRaw(`${today} = ?`, [true]);
+                });
+            });
+        });
+        const schedules = await schedulesQuery.exec();
+        schedules.forEach(async (schedule) => {
+            if (schedule.checkpoint.notificationAction === 'On Time') {
+                const scheduleTime = luxon_1.DateTime.fromFormat(schedule.startTime.toFormat('yyyy-MM-dd HH:mm:ss'), 'yyyy-MM-dd HH:mm:ss', { zone: 'UTC' }).toISO();
+                Bull_1.default.schedule(new SendScheduleNotification_1.default().key, schedule, (0, date_fns_1.parseISO)(scheduleTime));
+            }
+            else {
+                const scheduleTime = luxon_1.DateTime.fromFormat(schedule.startTime.minus({ minutes: 10 }).toFormat('yyyy-MM-dd HH:mm:ss'), 'yyyy-MM-dd HH:mm:ss', { zone: 'UTC' });
+                if (luxon_1.DateTime.now().diff(scheduleTime, 'seconds').seconds < 0) {
+                    Bull_1.default.schedule(new SendScheduleNotification_1.default().key, schedule, (0, date_fns_1.parseISO)(scheduleTime.toISO()));
+                }
+            }
+        });
+    }
+}
+exports.default = QueueScheduleNotification;
 //# sourceMappingURL=QueueScheduleNotification.js.map
--- app/Jobs/SendScheduleNotification.js
+++ app/Jobs/SendScheduleNotification.js
@@ -1,1 +1,38 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const User_1 = __importDefault(global[Symbol.for('ioc.use')]("App/Models/User"));
+const Fcm_1 = __importDefault(global[Symbol.for('ioc.use')]("App/Services/Fcm"));
+class SendScheduleNotification {
+    constructor() {
+        this.key = 'SendScheduleNotification';
+    }
+    async handle(job) {
+        const { data: schedule } = job;
+        const projectId = schedule.schedule.project_id;
+        const guards = await User_1.default.query().select(['id', 'device_token']).whereNotNull('device_token').whereHas('clientStaff', (query) => {
+            query.where('project_id', projectId);
+        }).whereHas('role', (query) => {
+            query.where('name', 'guard');
+        });
+        const guardTokens = guards.map(guard => guard.deviceToken);
+        Fcm_1.default.sendNotifications(guardTokens, {
+            payload: {
+                notification: {
+                    title: 'Upcoming Schedule',
+                    body: `You have upcoming schedule ${schedule.checkpoint.name}`
+                },
+                data: {
+                    type: 'upcoming_schedule',
+                    scheduleId: schedule.schedule_id.toString(),
+                    checkpoint: JSON.stringify(schedule.checkpoint),
+                }
+            }
+        });
+        return true;
+    }
+}
+exports.default = SendScheduleNotification;
 //# sourceMappingURL=SendScheduleNotification.js.map
